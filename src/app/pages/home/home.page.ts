162600ba3c88b2cf9763e08b28720010aa0494be
eideard-hm <edier.hernandez@samtel.co>
2021-12-23 16:08:11 -0500

    Basic app with ionic framework using angular

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Componente } from 'src/app/interfaces/component.interface';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  components: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
 
 }
